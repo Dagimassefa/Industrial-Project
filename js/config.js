74708edda4b9fe1119026beae5969c74ffadd27f
Dagimassefa <kuku.assefa18@gmail.com>
2022-12-16 17:43:29 +0300

    file for configuring the firebase and machine learning

---
+++ js/config.js
@@ -0,0 +1,11 @@
+
+var config = {
+    API_KEY: "AIzaSyC2BqWafwfRkjo7SssA0LFQluGyfFLZ6Ek",
+    AUTH_DOMAIN: "ai-bot-bbc9e.firebaseapp.com",
+    DATABASE_URL: 'https://ai-bot-bbc9e-default-rtdb.firebaseio.com/',
+    PROJECT_ID: "ai-bot-bbc9e",
+    STORAGE_BUCKET: "ai-bot-bbc9e.appspot.com",
+    MESSAGING_SENDER_ID: "660147107908",
+    APP_ID: "1:660147107908:web:7a08a5cf6600f6438b6411",
+    measurementId: "G-9MXF250ND8"
+};
